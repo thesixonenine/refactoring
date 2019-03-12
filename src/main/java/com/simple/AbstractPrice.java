533b871a8ff80dfc833bd1cec86e5e96e3ec7282
Simple <huyang1994822@gmail.com>
2019-03-12 17:43:03 +0800

    1.4 本章使用的重构手法: 1. Extract Method(提取函数) 2. Move Method(移动函数) 3. Replace Conditional with Polymorphism(用多态来替换条件) 4. Self Encapsulate Field(字段的自我封装) 5. Replace Type Code with State/Strategy(用状态模式或者策略模式来替换类型代码)
    重构的节奏: 测试-->小修改-->测试-->小修改-->测试-->小修改......

--- src/main/java/com/simple/AbstractPrice.java
+++ src/main/java/com/simple/AbstractPrice.java
@@ -21,11 +21,18 @@ public abstract class AbstractPrice {
     /**
      * 获得价格
      *
-     * @param daysRented 租赁市场
+     * @param daysRented 租赁时长
      * @return 价格
      */
     abstract BigDecimal getCharge(int daysRented);
 
+    /**
+     * 获得常客积分
+     * 提供默认的积分计算方法, 子类重写自己的计算规则
+     *
+     * @param daysRented 租赁时长
+     * @return 常客积分
+     */
     public int getFrequentRenterPoints(int daysRented) {
         return 1;
     }
