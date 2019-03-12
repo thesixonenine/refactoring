b46b9c893fb1b48b75a22cd7272899b18c733596
Simple <huyang1994822@gmail.com>
2019-03-12 15:59:49 +0800

    1.3

--- src/main/java/com/simple/Rental.java
+++ src/main/java/com/simple/Rental.java
@@ -75,7 +75,8 @@ public class Rental {
         if (getMovie().getPriceCode() == Movie.NEW_RELEASE &&
                 getDaysRented() > 1) {
             return 2;
+        } else {
+            return 1;
         }
-        return 1;
     }
 }
